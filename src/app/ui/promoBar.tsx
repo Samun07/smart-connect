058d01156438131b12c045441fcd4f51b1320c27
Samun07 <samuelsanchez.szr@gmail.com>
2024-07-21 23:23:54 -0500

    v1.2

--- src/app/ui/promoBar.tsx
+++ src/app/ui/promoBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PromoBar() {
   return (
-    <div className="flex flex-col md:flex-row gap-4 justify-center md:px-[14vw] px-1 items-center bg-[var(--color-secondary)] py-4">
+    <div className="flex flex-col md:flex-row gap-4 justify-center px-[14vw] items-center bg-[var(--color-secondary)] py-4">
       <span className="text-white">
         <strong>
         📍 Visit Our New Store at <a className="underline text-white" target="_blank" href="https://maps.app.goo.gl/4NosvSDSBTPJbGVP8">3029 N Roxboro St</a>! 🎉
