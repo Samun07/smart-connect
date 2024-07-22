dc12b5a08d92b08a1edcba8927cb7520d64fc33e
Samun07 <samuelsanchez.szr@gmail.com>
2024-07-21 23:42:42 -0500

    v1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -459,6 +459,138 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 bg-white rounded-lg overflow-hidden py-4 px-8 border border-solid border-gray-100">
+          <div className="flex justify-center items-center m-4">
+            <img
+              src="https://www.pngall.com/wp-content/uploads/15/iPhone-Xr-PNG-Image-HD.png"
+              alt=""
+              className="object-contain h-48"
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <h1 className="text-lg font-bold">iPhone XR</h1>
+            <ul className="flex flex-col list-none">
+              <li>64GB</li>
+              <li>Unlocked</li>
+              <li>A-Stock-OEM Box</li>
+            </ul>
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-1 items-center whitespace-nowrap">
+              <span className="font-bold">$250</span>
+              -
+              <span>In-Stock</span>
+              </div>
+
+              <div className="grid xl:grid-cols-2 grid-cols-1 gap-4 justify-center items-center">
+                <a
+                  href="https://maps.app.goo.gl/4NosvSDSBTPJbGVP8"
+                  target="_blank"
+                  className="flex justify-center bg-[var(--color-secondary)] py-3 px-5 rounded-xl  font-semibold text-base text-white
+          transition-colors hover:bg-[var(--hover-color-secondary)] whitespace-nowrap"
+                >
+                  Visit Us
+                </a>
+                <a
+                  href="https://wa.me/+19842608676"
+                  target="_blank"
+                  className="flex justify-center bg-[var(--color-primary)] py-3 px-5 rounded-xl  font-semibold text-base text-[color:var(--text-color-primary)]
+          transition-colors hover:bg-[var(--hover-color-primary)] whitespace-nowrap"
+                >
+                  Write To Us
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 bg-white rounded-lg overflow-hidden py-4 px-8 border border-solid border-gray-100">
+          <div className="flex justify-center items-center m-4">
+            <img
+              src="https://www.clevercel.co/cdn/shop/products/iphone_8_plus_700x_77e43ee7-3cec-4eea-855b-a0299a45204f.png?v=1665798983"
+              alt=""
+              className="object-contain h-48"
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <h1 className="text-lg font-bold">iPhone 8 Plus</h1>
+            <ul className="flex flex-col list-none">
+              <li>64GB</li>
+              <li>Unlocked</li>
+              <li>A-Stock-OEM Box</li>
+            </ul>
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-1 items-center whitespace-nowrap">
+              <span className="font-bold">$240</span>
+              -
+              <span>In-Stock</span>
+              </div>
+
+              <div className="grid xl:grid-cols-2 grid-cols-1 gap-4 justify-center items-center">
+                <a
+                  href="https://maps.app.goo.gl/4NosvSDSBTPJbGVP8"
+                  target="_blank"
+                  className="flex justify-center bg-[var(--color-secondary)] py-3 px-5 rounded-xl  font-semibold text-base text-white
+          transition-colors hover:bg-[var(--hover-color-secondary)] whitespace-nowrap"
+                >
+                  Visit Us
+                </a>
+                <a
+                  href="https://wa.me/+19842608676"
+                  target="_blank"
+                  className="flex justify-center bg-[var(--color-primary)] py-3 px-5 rounded-xl  font-semibold text-base text-[color:var(--text-color-primary)]
+          transition-colors hover:bg-[var(--hover-color-primary)] whitespace-nowrap"
+                >
+                  Write To Us
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 bg-white rounded-lg overflow-hidden py-4 px-8 border border-solid border-gray-100">
+          <div className="flex justify-center items-center m-4">
+            <img
+              src="https://saldos.clevercel.co/cdn/shop/products/iPhone_SE_1d12f8eb-a5c7-4b29-a6b2-bcaf3b674e59_1800x.png?v=1661205420"
+              alt=""
+              className="object-contain h-48"
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <h1 className="text-lg font-bold">iPhone SE Gen 2</h1>
+            <ul className="flex flex-col list-none">
+              <li>64GB</li>
+              <li>Unlocked</li>
+              <li>A-Stock-OEM Box</li>
+            </ul>
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-1 items-center whitespace-nowrap">
+              <span className="font-bold">$199</span>
+              -
+              <span>In-Stock</span>
+              </div>
+
+              <div className="grid xl:grid-cols-2 grid-cols-1 gap-4 justify-center items-center">
+                <a
+                  href="https://maps.app.goo.gl/4NosvSDSBTPJbGVP8"
+                  target="_blank"
+                  className="flex justify-center bg-[var(--color-secondary)] py-3 px-5 rounded-xl  font-semibold text-base text-white
+          transition-colors hover:bg-[var(--hover-color-secondary)] whitespace-nowrap"
+                >
+                  Visit Us
+                </a>
+                <a
+                  href="https://wa.me/+19842608676"
+                  target="_blank"
+                  className="flex justify-center bg-[var(--color-primary)] py-3 px-5 rounded-xl  font-semibold text-base text-[color:var(--text-color-primary)]
+          transition-colors hover:bg-[var(--hover-color-primary)] whitespace-nowrap"
+                >
+                  Write To Us
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
         
       </section>
     </main>
